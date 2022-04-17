e5df3b54a67bf3f636797770a57423f404f4ceab
Dexter Volkman <dexter.volkman@protonmail.com>
2022-04-16 23:05:17 -0700

    Testing dialogic

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -4,6 +4,7 @@ extends KinematicBody2D
 export var speed = 75
 
 var _velocity = Vector2.ZERO
+var new_dialog = Dialogic.start("BillIntroduction")
 
 
 func _physics_process(_delta) -> void:
@@ -11,6 +12,11 @@ func _physics_process(_delta) -> void:
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	)
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		add_child(new_dialog)
+	
 	if direction.length() > 1.0:
 		direction = direction.normalized()
+		
 	move_and_slide(speed * direction)
